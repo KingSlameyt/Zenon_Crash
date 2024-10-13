f1764ceb6268a6c1ef3bf2e099af869b36c723a6
KingSlameyt <107631912+KingSlameyt@users.noreply.github.com>
2024-10-14 00:14:42 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "4917528216491"
+global.ownernomer = "1(930)242-1710"
 global.ownername = "Avast"
 global.ytname = "Toxxic Md"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '4917528216491'  //creator number
+global.ownernumber = '1(930)242-1710'  //creator number
 global.ownername = 'Avast' //owner name
 global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
 
